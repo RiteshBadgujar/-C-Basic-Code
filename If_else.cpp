40046c2674ab41721d3ee591260fc246338a424d
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-29 21:29:45 +0530

    [ADDED]:Ritesh  The code using for If Else Statement

---
+++ If_else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Enter the no :";
+	cin>>a;
+	
+	if(18 <= a){
+		cout<<"Alllow for voting";
+	}
+	else{
+		cout<<"not Allowed for voting";
+	}
+}
+
