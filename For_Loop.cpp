477481e8f1c8e74567c15c02ec22068b943ecdd8
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-31 21:33:53 +0530

    [ADDED]:Ritesh  The Using For loop in code

---
+++ For_Loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int f;
+	 
+	 cout<<"Enter the no :";
+	 cin>>f;
+	 
+	 for(int i=1; i<=f; i++)
+	 {
+	 	
+	 	cout<<i<<endl;
+	 	
+	 }
+	 
+	 
+}
