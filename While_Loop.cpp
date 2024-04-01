45331ac4ed85c849043d297bcb4c66b18bcbffa3
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-04-01 21:50:34 +0530

    [ADDED]:Ritesh  The Using While loop in code

---
+++ While_Loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int f;
+	int i;
+	 
+	 cout<<"Enter the no :";
+	 cin>>f;
+	 
+	 while(i<=f)
+	 {
+	 	
+	 	cout<<i<<endl;
+	 	i++;
+	 	
+	 }
+	 
+	 
+}
