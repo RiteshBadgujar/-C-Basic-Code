df61e020972c6cc4fe2d68b072db1a58c21da5ee
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-04-02 21:28:55 +0530

    [ADDED]:Ritesh  The Using Do While loop in a code

---
+++ Do_While_Loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int f;
+	int i;
+	 
+	 cout<<"Enter the no :";
+	 cin>>f;
+	 
+	 do
+	 {
+	 	
+	 	cout<<i<<endl;
+	 	i++;
+	 	
+	 }
+	 while(i<=f);
+	 
+	 
+}
